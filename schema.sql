a5ceea0790ae73ef9f47a3325ef4243da6e2ca4e
isyumich <59385945+Isyumich@users.noreply.github.com>
2020-10-26 18:24:10 +0300

    Исправления по замечаниям наставника

--- schema.sql
+++ schema.sql
@@ -7,8 +7,7 @@ use yeticave;
 create table category (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(64),
-  symbol_code VARCHAR(64),
-  INDEX (id)
+  symbol_code VARCHAR(64)
 );
 
 create table lot (
@@ -22,11 +21,10 @@ create table lot (
   bid_step INT,
   author BIGINT,
   winner BIGINT,
-  category BIGINT,
+  category_id BIGINT,
   INDEX (author),
   INDEX (winner),
-  INDEX (category),
-  UNIQUE INDEX (id),
+  INDEX (category_id),
   INDEX (name)
 );
 
@@ -34,35 +32,33 @@ create table bid (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   date TIMESTAMP,
   amount INT,
-  user BIGINT,
-  lot BIGINT,
-  UNIQUE INDEX (id),
-  INDEX (user),
-  INDEX (lot)
+  user_id BIGINT,
+  lot_id BIGINT,
+  INDEX (user_id),
+  INDEX (lot_id)
 );
 
 create table user (
   id BIGINT AUTO_INCREMENT PRIMARY KEY,
   register_date TIMESTAMP,
   email VARCHAR(128) UNIQUE,
-  name VARCHAR(64) UNIQUE,
+  name VARCHAR(64),
   password CHAR(64),
   contact VARCHAR(64),
-  bid BIGINT,
-  lot BIGINT,
-  UNIQUE INDEX (id),
-  UNIQUE INDEX (email),
-  INDEX (bid),
-  INDEX (lot)
+  bid_id BIGINT,
+  lot_id BIGINT,
+  UNIQUE INDEX (name),
+  INDEX (bid_id),
+  INDEX (lot_id)
 );
 
 ALTER TABLE lot ADD FOREIGN KEY (author) REFERENCES user (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 ALTER TABLE lot ADD FOREIGN KEY (winner) REFERENCES user (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE lot ADD FOREIGN KEY (category) REFERENCES category (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE bid ADD FOREIGN KEY (user) REFERENCES user (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE bid ADD FOREIGN KEY (lot) REFERENCES lot (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE user ADD FOREIGN KEY (lot) REFERENCES lot (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
-ALTER TABLE user ADD FOREIGN KEY (bid) REFERENCES bid (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE lot ADD FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE bid ADD FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE bid ADD FOREIGN KEY (lot_id) REFERENCES lot (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE user ADD FOREIGN KEY (lot_id) REFERENCES lot (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE user ADD FOREIGN KEY (bid_id) REFERENCES bid (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 
 
